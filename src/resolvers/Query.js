d710b8fca248af21c00a4c9c6445bb135add1a1f
Diemas Michiels <michielsdiemas@gmail.com>
2019-01-01 16:09:45 +0100

    Module 8: Credit Card Checkout

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -24,6 +24,36 @@ const Query = {
     hasPermission(ctx.request.user, ['ADMIN', 'PERMISSIONUPDATE'])
     // if they do query all the users
     return ctx.db.query.users({}, info)
+  },
+  async order (parent, args, ctx, info) {
+    // make sure they are logged in
+    if (!ctx.request.userId) {
+      throw new Error('You must be logged in')
+    }
+    // query the current user
+    const order = await ctx.db.query.order({
+      where: { id: args.id }
+    }, info)
+    // check if they have permissions to see this order
+    const ownsOrder = order.user.id === ctx.request.userId
+    const hasPermissionToSeeOrder = ctx.request.user.permissions.includes('ADMIN')
+    if (!ownsOrder || !hasPermissionToSeeOrder) {
+      throw new Error('You do not have permissions to see this order')
+    }
+    // return the order
+    return order
+  },
+  async orders (parent, args, ctx, info) {
+    const { userId } = ctx.request
+    // make sure they are logged in
+    if (!userId) {
+      throw new Error('You must be logged in')
+    }
+    return ctx.db.query.orders({
+      where: {
+        user: { id: userId }
+      }
+    }, info)
   }
 }
 
